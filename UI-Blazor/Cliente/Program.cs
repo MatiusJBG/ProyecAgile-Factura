e6daafc4e3cf2a248ea199a7e2d47e5ffc0c5dba
MatiusJBG <benjaaigaje01@gmail.com>
2025-11-13 14:30:58 -0500

    Creando correctamente lo necesario, y corrigiendo errores de Blazor, por una mala instalacion

--- UI-Blazor/Cliente/Program.cs
+++ UI-Blazor/Cliente/Program.cs
@@ -1,34 +1,11 @@
-// --- Using statements for the Client (Blazor WebAssembly) ---
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System;
-using System.Net.Http;
-using Microsoft.Extensions.DependencyInjection;
+using Cliente;
 
-// Make sure this namespace matches your Client project's name
-using Cliente; 
-
-// 1. --- Create the application builder ---
-// Use WebAssemblyHostBuilder for browser-based apps
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-// 2. --- Register root components ---
-// Finds <div id="app"> in wwwroot/index.html
-builder.RootComponents.Add<App>("#app"); 
-// Adds support for <PageTitle> and <HeadContent> components
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// 3. --- Register services with the container (for the client) ---
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// Configure the HttpClient to point to the server's base address
-// This allows you to make API calls (e.g., http.GetAsync("/api/clientes"))
-builder.Services.AddScoped(sp => new HttpClient 
-{ 
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
-});
-
-// (You can add other client-side services here, like MudBlazor, etc.)
-
-
-// 4. --- Build and run the client application ---
 await builder.Build().RunAsync();
